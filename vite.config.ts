2be71dea23acfda05ebc172886691ec4e0955f27
raquelvalentina <raquelvalentinaherrera@gmail.com>
2026-03-24 18:50:29 +0100

    improve

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import react from '@vitejs/react-swc' 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
       tailwindcss(),],
+  base: '/racheldev.github.io/',
 })
